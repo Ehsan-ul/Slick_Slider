baebbb144805541a6f7839a2afbeb3a6c2741e41
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-04 01:39:22 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.slider1').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 4000,
+    cssEase: 'linear',
+    dots: true,
+  });
+});
