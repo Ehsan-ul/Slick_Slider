dc1fb670cc4745252dd945beb4bd18d3f5b92bf3
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-04 16:27:05 +0600

    slick slider part 2

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
+  // slider 1 js
   $('.slider1').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 1500,
     fade: true,
-    speed: 4000,
+    speed: 2000,
+    cssEase: 'linear',
+    dots: true,
+  });
+
+  // slider 2 js
+  $('.slider2').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    fade: true,
+    speed: 2000,
     cssEase: 'linear',
     dots: true,
   });
